7d69bd7bc9fda2abee204c0bc541047d1da18ddb
mtrempoltsev <mt@devexp.ru>
2018-02-14 09:26:01 +0300

    fix token

--- ci/travis/after_success-linux.sh
+++ ci/travis/after_success-linux.sh
@@ -1,4 +1,5 @@
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info '/usr/*' '*/externals/*' --output-file coverage.info
 lcov --list coverage.info
-coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
+echo coveralls-lcov --repo-token=${COVERALLS_REPO_TOKEN} coverage.info
+coveralls-lcov --repo-token=${COVERALLS_REPO_TOKEN} coverage.info
