d6406bba7939d4a19569028e538edca081f74280
mtrempoltsev <mt@devexp.ru>
2018-02-14 09:35:05 +0300

    hide coverall token

--- ci/travis/after_success-linux.sh
+++ ci/travis/after_success-linux.sh
@@ -1,5 +1,4 @@
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info '/usr/*' '*/externals/*' --output-file coverage.info
 lcov --list coverage.info
-echo coveralls-lcov --repo-token=${COVERALLS_REPO_TOKEN} coverage.info
 coveralls-lcov --repo-token=${COVERALLS_REPO_TOKEN} coverage.info
