4f7b8fdf4e785f864e7bad66da37d23da686e9c7
mtrempoltsev <mt@devexp.ru>
2018-02-14 08:19:08 +0300

    coveralls

--- ci/travis/after_success-linux.sh
+++ ci/travis/after_success-linux.sh
@@ -1,1 +1,4 @@
-coveralls --root . -E ".*gtest.*" -E ".*CMakeFiles.*" -E "../externals/*"
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info 'tests/*' '/usr/*' '../externals/*' '*gtest.*' '*CMakeFiles.*"' --output-file coverage.info
+lcov --list coverage.info
+coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -24,4 +24,7 @@ if [ "$TARGET_CPU" == "x86" ]; then
     sudo apt-get install -y g++-5-multilib
 fi
 
-pip install --user cpp-coveralls
+wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
+tar xf lcov_1.11.orig.tar.gz
+sudo make -C lcov-1.11/ install
+gem install coveralls-lcov
