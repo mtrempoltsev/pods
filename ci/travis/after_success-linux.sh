67ecb39edaefa67ee69e500875d03657f3d999bf
mtrempoltsev <mt@devexp.ru>
2018-02-14 10:19:49 +0300

    cleanup ci scripts

--- ci/travis/after_success-linux.sh
+++ ci/travis/after_success-linux.sh
@@ -1,4 +1,6 @@
-lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info '/usr/*' '*/externals/*' --output-file coverage.info
-lcov --list coverage.info
-coveralls-lcov --repo-token=${COVERALLS_REPO_TOKEN} coverage.info
+if [ "$PODS_COVERAGE" = "YES" ]; then
+    lcov --directory . --capture --output-file coverage.info
+    lcov --remove coverage.info '/usr/*' '*/externals/*' --output-file coverage.info
+    lcov --list coverage.info
+    coveralls-lcov --repo-token=${COVERALLS_REPO_TOKEN} coverage.info
+fi
