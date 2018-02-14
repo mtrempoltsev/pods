d9c53a810e725169a9c70d3b6f1d80d3cbf18ed5
mtrempoltsev <mt@devexp.ru>
2018-02-14 09:08:04 +0300

    lcov

--- ci/travis/after_success-linux.sh
+++ ci/travis/after_success-linux.sh
@@ -1,4 +1,4 @@
 lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info 'tests/*' '/usr/*' '../externals/*' '*gtest.*' '*CMakeFiles.*"' --output-file coverage.info
+lcov --remove coverage.info '/usr/*' '*/externals/*' --output-file coverage.info
 lcov --list coverage.info
 coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
