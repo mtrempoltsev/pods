f02075885032587be49dec0a31493cb5027f830b
mtrempoltsev <mt@devexp.ru>
2018-04-20 18:03:41 +0300

    add byte-order utils

---
+++ include/pods/details/endianness.h
@@ -0,0 +1,105 @@
+﻿#pragma once
+
+namespace pods
+{
+    namespace details
+    {
+#ifdef PODS_BIG_ENDIAN
+        static constexpr bool BigEndian = true;
+        static constexpr bool LittleEndian = false;
+
+        template <class T>
+        T toBigEndian(T value) noexcept
+        {
+            return value;
+        }
+
+    #ifdef _MSC_VER
+        template <class T, typename std::enable_if<sizeof(T) == 2, int>::type = 0>
+        T toLittleEndian(T value) noexcept
+        {
+            return _byteswap_ushort(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 4, int>::type = 0>
+        T toLittleEndian(T value) noexcept
+        {
+            return _byteswap_ulong(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 8, int>::type = 0>
+        T toLittleEndian(T value) noexcept
+        {
+            return _byteswap_uint64(value);
+        }
+    #else
+        template <class T, typename std::enable_if<sizeof(T) == 2, int>::type = 0>
+        T toLittleEndian(T value) noexcept
+        {
+            return __builtin_bswap16(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 4, int>::type = 0>
+        T toLittleEndian(T value) noexcept
+        {
+            return __builtin_bswap32(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 8, int>::type = 0>
+        T toLittleEndian(T value) noexcept
+        {
+            return __builtin_bswap64(value);
+        }
+    #endif
+#endif
+
+#ifdef PODS_LITTLE_ENDIAN
+        static constexpr bool BigEndian = false;
+        static constexpr bool LittleEndian = true;
+
+        template <class T>
+        T toLittleEndian(T value) noexcept
+        {
+            return value;
+        }
+
+    #ifdef _MSC_VER
+        template <class T, typename std::enable_if<sizeof(T) == 2, int>::type = 0>
+        T toBigEndian(T value) noexcept
+        {
+            return _byteswap_ushort(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 4, int>::type = 0>
+        T toBigEndian(T value) noexcept
+        {
+            return _byteswap_ulong(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 8, int>::type = 0>
+        T toBigEndian(T value) noexcept
+        {
+            return _byteswap_uint64(value);
+        }
+    #else
+        template <class T, typename std::enable_if<sizeof(T) == 2, int>::type = 0>
+        T toBigEndian(T value) noexcept
+        {
+            return __builtin_bswap16(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 4, int>::type = 0>
+        T toBigEndian(T value) noexcept
+        {
+            return __builtin_bswap32(value);
+        }
+
+        template <class T, typename std::enable_if<sizeof(T) == 8, int>::type = 0>
+        T toBigEndian(T value) noexcept
+        {
+            return __builtin_bswap64(value);
+        }
+    #endif
+#endif
+    }
+}
---
+++ tests/test_endianness.cpp
@@ -0,0 +1,45 @@
+﻿#include <gtest/gtest.h>
+
+#include <pods/details/endianness.h>
+
+#ifdef PODS_BIG_ENDIAN
+    #define PODS_PASS pods::details::toBigEndian
+    #define PODS_CONV pods::details::toLittleEndian
+#else
+    #define PODS_PASS pods::details::toLittleEndian
+    #define PODS_CONV pods::details::toBigEndian
+#endif
+
+template <class T>
+void check(T from, T expected)
+{
+    const auto a1 = PODS_PASS(from);
+    EXPECT_EQ(from, a1);
+
+    const auto a2 = PODS_CONV(from);
+    EXPECT_EQ(expected, a2);
+
+    const auto a3 = PODS_CONV(a2);
+    EXPECT_EQ(from, a3);
+}
+
+TEST(testEndianess, common)
+{
+#ifdef PODS_BIG_ENDIAN
+    EXPECT_TRUE(pods::details::BigEndian);
+    EXPECT_FALSE(pods::details::LittleEndian);
+#else
+    EXPECT_TRUE(pods::details::LittleEndian);
+    EXPECT_FALSE(pods::details::BigEndian);
+#endif
+
+    check<int16_t>(0x0102, 0x0201);
+    check<uint16_t>(0x0102, 0x0201);
+    check<int32_t>(0x01020304, 0x04030201);
+    check<uint32_t>(0x01020304, 0x04030201);
+    check<int64_t>(0x0102030405060708, 0x0807060504030201);
+    check<uint64_t>(0x0102030405060708, 0x0807060504030201);
+}
+
+#undef PODS_PASS
+#undef PODS_CONV
